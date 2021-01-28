d522809f0f3ce3b46ca2a098f36417e38051d59e
amanda <amanda-jose@live.com>
2021-01-28 09:52:23 -0500

    added the student address (fixed typo)

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -10,11 +10,19 @@ package studentlist;
  * @author Amanda
  */
     public class Student {
-       private String name; 
+       private String name;
+       private String address;
 
         public String getName() {
             return name;
         }
+        
+
+        public String getAddress() {
+            return address;
+        }
+        
+        
 
         public void setName(String name) {
             this.name = name;
